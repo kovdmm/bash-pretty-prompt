7320e2a0f489145b271f979e914f6ea431542d2e
Dmitry Kovalev <kov.dmm@gmail.com>
2025-07-03 21:27:27 +0500

    Refactor: remove git prompt dependency, add hooks, improve git info
    - Add `__bpp_before_exec` and __bpp_after_exec hook calls
    - Remove `__git_ps1` dependency from prompt
    - Add `__bpp_git_branch_simple` for minimal git branch display
    - Rename `__bpp_git_branch_formatted` to `__bpp_git_branch_pretty` and update usage
    - Add `__bpp_msystem` for MSYSTEM display in simple theme, if defined (for Windows)
    - Minor stylistic and formatting improvements

--- pretty-prompt.sh
+++ pretty-prompt.sh
@@ -3,31 +3,40 @@
 BPP_ROOT="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 source "$BPP_ROOT/globals.sh"
 
-__bpp_git_dirty() {
-  [[ -n "$(git status --porcelain 2>/dev/null)" ]] && echo '*'
-}
+__bpp_before_exec 2>/dev/null
 
 __bpp_git_remote() {
   if [[ -n "$(git remote 2>/dev/null)" ]]; then
     local remotes
     remotes=$(git remote -v 2>/dev/null)
     if [[ "$remotes" == *github.com* ]]; then
-      echo "$BPP_GITHUB_ICON"
+      echo -n "$BPP_GITHUB_ICON"
     elif [[ "$remotes" == *bitbucket.org* ]]; then
-      echo "$BPP_BITBUCKET_ICON"
+      echo -n "$BPP_BITBUCKET_ICON"
     else
-      echo "$BPP_GITLAB_ICON"
+      echo -n "$BPP_GITLAB_ICON"
     fi
+    echo -n " "
   fi
+}
+
+__bpp_git_dirty() {
+  [[ -n "$(git status --porcelain 2>/dev/null)" ]] && echo '*'
 }
 
 __bpp_git_branch() {
   git rev-parse --abbrev-ref HEAD 2>/dev/null
 }
 
-__bpp_git_branch_formatted() {
-  if [[ "$(__bpp_git_branch)" != "" ]]; then
-    echo -e "${BBP_GIT_BG}${BPP_SEP_R}${BPP_MAIN_FG} $(__bpp_git_remote) ${BPP_BRANCH_ICON} $(__bpp_git_branch)$(__bpp_git_dirty) ${BPP_RESET}${BBP_GIT_FG}"
+__bpp_git_branch_simple() {
+  if [[ -n "$(__bpp_git_branch)" ]]; then
+    echo " ($(__bpp_git_branch)$(__bpp_git_dirty))"
+  fi
+}
+
+__bpp_git_branch_pretty() {
+  if [[ -n "$(__bpp_git_branch)" ]]; then
+    echo -e "${BBP_GIT_BG}${BPP_SEP_R}${BPP_MAIN_FG} $(__bpp_git_remote)${BPP_BRANCH_ICON} $(__bpp_git_branch)$(__bpp_git_dirty) ${BPP_RESET}${BBP_GIT_FG}"
   else
     echo -e "${BPP_RESET}${BPP_CWD_FG}"
   fi
@@ -37,37 +46,43 @@ __bpp_title() {
   if [[ -n "$BPP_TITLE" ]]; then
     printf "\e]0;%s\007" "$BPP_TITLE"
   else
-    printf "\e]0;%s\007" "$BPP_TITLE"
+    printf "\e]0;%s\007" "$TITLEPREFIX: $PWD"
+  fi
+}
+
+__bpp_msystem() {
+  if [[ -n $MSYSTEM ]]; then
+    printf "\[\e[35m\]%s " "$MSYSTEM"
   fi
 }
 
 __bpp_theme_simple() {
   PS1="\`__bpp_title\`\n\
-\[\e[32m\]\u@\h \[\e[35m\]${MSYSTEM} \[\e[33m\]\w\[\e[36m\]\`__git_ps1\`${BPP_RESET}\n\
+\[\e[32m\]\u@\h $(__bpp_msystem)\[\e[33m\]\w\[\e[36m\]\`__bpp_git_branch_simple\`${BPP_RESET}\n\
 $ "
 }
 
 __bpp_theme_pretty() {
   PS1="\`__bpp_title\`\n\
 ${BPP_CWD_BG}${BPP_MAIN_FG} ${BPP_FOLDER_ICON} \w ${BPP_RESET}${BPP_CWD_FG}\
-\`__bpp_git_branch_formatted\`${BPP_SEP_R}${BPP_RESET}\n\
+\`__bpp_git_branch_pretty\`${BPP_SEP_R}${BPP_RESET}\n\
 $ "
 }
 
 __bpp_theme_minimalistic() {
   PS1="\`__bpp_title\`\n\
 ${BPP_MAIN_FG}${BPP_CWD_BG} ${BPP_FOLDER_ICON} \w ${BPP_CWD_FG}\
-\`__bpp_git_branch_formatted\`${BPP_SEP_R}\n\
+\`__bpp_git_branch_pretty\`${BPP_SEP_R}\n\
 \[${BBP_TIME_BG}${BPP_MAIN_FG}\] $ \[${BPP_RESET}${BBP_TIME_FG}\]${BPP_SEP_R}\[${BPP_RESET}\] "
 }
 
 __bpp_theme_involved() {
   PS1="\`__bpp_title\`\n\
 ╭${BPP_CWD_FG}${BPP_SEP_L}${BPP_CWD_BG}${BPP_MAIN_FG} ${BPP_FOLDER_ICON} \w ${BPP_RESET}${BPP_CWD_FG}\
-\`__bpp_git_branch_formatted\`${BBP_TIME_BG}${BPP_SEP_R}\
+\`__bpp_git_branch_pretty\`${BBP_TIME_BG}${BPP_SEP_R}\
 ${BPP_MAIN_FG} ${BPP_CLOCK_ICON} \`date +%H:%M\` ${BPP_RESET}${BBP_TIME_FG}${BPP_SEP_R}\
 \[${BPP_RESET}\]\n\
-╰┈➤  "
+╰┈➤ "
 }
 
 # setup theme
@@ -79,3 +94,5 @@ if [[ -n "$BPP_THEME" ]]; then
 else
   __bpp_theme_pretty
 fi
+
+__bpp_after_exec 2>/dev/null
